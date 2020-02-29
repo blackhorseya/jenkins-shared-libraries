6fadb8db7ba5ba2df0bc4fec410cb8adb91ee5d4
Sean Cheng <blackhorseya@gmail.com>
2020-02-29 19:39:57 +0800

    feat implement commit build for dotnet

---
+++ vars/dotnetCommit.groovy
@@ -0,0 +1,53 @@
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    pipeline {
+        agent {
+            kubernetes {
+                yaml """
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: dotnet-sdk
+    image: blackhorseya/dotnet-builder:3.1-alpine
+    command:
+    - cat
+    tty: true
+"""
+            }
+        }
+        stages {
+            stage('Prepare') {
+                steps {
+                    echo "branch name: ${env.GIT_BRANCH}"
+                    sh 'ls -al'
+                    sh 'printenv'
+                }
+            }
+
+            stage('Build') {
+                steps {
+                    container('dotnet-sdk') {
+                        sh '''
+                        dotnet build -c Release -o ./publish
+                        '''
+                    }
+                }
+            }
+
+            stage('Test') {
+                steps {
+                    container('dotnet-sdk') {
+                        sh '''
+                        dotnet test
+                        '''
+                    }
+                }
+            }
+        }
+    }
+}
