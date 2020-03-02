5aa2e3bfbbe71c02ef1fa938cd0f6b50085af0c6
Sean Cheng <blackhorseya@gmail.com>
2020-03-02 12:06:42 +0800

    Create dotnet.groovy

---
+++ vars/dotnet.groovy
@@ -0,0 +1,25 @@
+def printInfo() {
+    sh label: "print dotnet info", script: "dotnet --info"
+}
+
+def build(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    def nugetSource = ""
+    def useCache = ""
+
+    if (!config.nuget?.trim()) {
+        nugetSource = "--source ${config.nuget}"
+    }
+
+    if (!config.useCache) {
+        useCache = "--no-cache"
+    }
+
+    sh label: "dotnet core build", script: """
+    dotnet build -v normal --nologo ${nugetSource} ${useCache}
+    """
+}
