59452d899a5a4dcdb1f10a83d9762bba231daa40
Sean Cheng <sean_cheng@trendmicro.com>
2020-03-02 16:41:47 +0800

    fix dotnet test output report path

--- vars/dotnet.groovy
+++ vars/dotnet.groovy
@@ -25,7 +25,7 @@ def test(Map configs) {
     def output = ""
 
     if (configs.genCoverage) {
-        coverage = "/p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=report/coverage.xml"
+        coverage = "/p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=reports/coverage.xml"
     }
 
     if (configs.genReport) {
