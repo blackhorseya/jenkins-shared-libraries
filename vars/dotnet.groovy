39521197d68172d1e424dc914249a90017a4d25f
Sean Cheng <blackhorseya@gmail.com>
2020-03-02 13:47:04 +0800

    Update dotnet.groovy

--- vars/dotnet.groovy
+++ vars/dotnet.groovy
@@ -24,25 +24,21 @@ def build(body) {
     """
 }
 
-def test(body) {
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-
+def test(Map configs) {
     def coverage = ""
     def report = ""
     def output = ""
 
-    if (config.genCoverage) {
+    if (configs.genCoverage) {
+        echo "gen coverage"
         coverage = "/p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=${PWD}/coverage/"
     }
 
-    if (config.genReport) {
+    if (configs.genReport) {
         report = "--logger trx -r ./TestResults/report.trx"
     }
 
-    if (config.output) {
+    if (configs.output) {
         output = "-o ./publish --no-build --no-restore"
     }
     
